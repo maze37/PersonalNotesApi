7895752b82776fb84a49e6ed7c28db22f52ee611
maze37 <91159480+maze37@users.noreply.github.com>
2026-01-18 11:24:53 +0300

    Update Note.cs

--- Models/Note.cs
+++ Models/Note.cs
@@ -7,7 +7,7 @@ public class Note
     public string Content { get; set; } = null!;
     public DateTime CreatedAt { get; set; }
 
-    public int UserId { get; set; }   // связь
+    public int UserId { get; set; }
     public User User { get; set; } = null!;
 
     public List<NoteTag> NoteTags { get; set; } = new();
